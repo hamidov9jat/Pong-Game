6b59dd31af8156ba7d53f94f9e334d55862181ce
Nijat Hamidov <hamidov9jat@gmail.com>
2022-09-08 22:15:01 +0400

    working paddles

--- main.py
+++ main.py
@@ -1,16 +1,19 @@
 from turtle import Screen
+from paddle import Paddle
 
 screen = Screen()
 screen.bgcolor('white')
-screen.setup(width=.75, height=.5)
+screen.setup(width=1280, height=720)
 screen.title('Pong Game')
 
+r_paddle = Paddle(position=(-590, 0))
+l_paddle = Paddle(position=(590, 0))
 
 screen.listen()
-# screen.onkeypress(key='w', fun=sn.up)
-# screen.onkeypress(key='s', fun=sn.down)
-# screen.onkeypress(key='a', fun=sn.left)
-# screen.onkeypress(key='d', fun=sn.right)
+screen.onkeypress(key='w', fun=r_paddle.go_up)
+screen.onkeypress(key='s', fun=r_paddle.go_down)
+screen.onkeypress(key='Up', fun=l_paddle.go_up)
+screen.onkeypress(key='Down', fun=l_paddle.go_down)
 
 
 
---
+++ paddle.py
@@ -0,0 +1,18 @@
+from turtle import Turtle
+
+
+class Paddle(Turtle):
+    def __init__(self, position):
+        super().__init__()
+        self.speed(0)
+        self.shape('square')
+        self.color('black')
+        self.shapesize(stretch_wid=9, stretch_len=1)
+        self.penup()
+        self.goto(position)
+
+    def go_up(self):
+        self.goto(self.xcor(), self.ycor() + 20)
+
+    def go_down(self):
+        self.goto(self.xcor(), self.ycor() - 20)
